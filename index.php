bdbdc4aa93b2a51afe5323fc7b80fecba764a3c8
Yusuf Supriyanto <yuutech@users.noreply.github.com>
2020-08-07 05:25:26 +0700

    make it better3

--- index.php
+++ index.php
@@ -53,6 +53,20 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
+	switch ($_SERVER['HTTP_HOST']) {
+		case 'ansora.test':
+			$env = 'development';
+			break;
+		case 'http://localhost/backend_ansora/':
+			$env = 'testing';
+			break;
+		case 'ansora.herokuapp.com':
+			$env = 'production';
+			break;
+		default:
+			$env = 'development';
+			break;
+	}
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
