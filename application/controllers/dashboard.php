563c7f001a8f49dbbc77e68c47268fc0590637bc
Danar Noverawan <39963063+danarn17@users.noreply.github.com>
2020-08-07 21:34:12 +0700

    fucek

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -6,7 +6,13 @@ class Dashboard extends CI_Controller
     function __construct()
     {
         parent::__construct();
+        $this->load->model('model_buku');
+        $this->load->model('invoice_m');
         $this->load->model('user_m');
+        $this->load->model('category_m');
+        $this->load->model('penerbit_m');
+        $this->load->model('count_m');
+        $this->load->model('product_model');
         if (!isset($this->session->userdata['userid'])) {
             $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show"
             role="alert">Anda Belum Login!<button type="button" class="close"
