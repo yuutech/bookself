b6c273961ef3f97df210aa18f7b63ce8fef68fce
Danar Noverawan <39963063+danarn17@users.noreply.github.com>
2020-08-07 21:07:56 +0700

    fucek

--- application/controllers/House.php
+++ application/controllers/House.php
@@ -4,18 +4,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
 date_default_timezone_set('Asia/Jakarta');
 class House extends CI_Controller
 {
-    function __construct()
-    {
-        parent::__construct();
-        $this->load->model('model_buku');
-        $this->load->model('user_m');
-    }
+    // function __construct()
+    // {
+    //     parent::__construct();
+    //     $this->load->model('model_buku');
+    //     $this->load->model('user_m');
+    // }
 
     public function index()
     {
-        $buku = $this->model_buku->tampildata()->result();
+        // $buku = $this->model_buku->tampildata()->result();
         $data = array(
-            'buku' => $buku,
+            'buku' => 'anjeng',
         );
         $this->load->view('template/header');
         $this->load->view('index', $data);
