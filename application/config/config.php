47ad86ac5eebb3a87b86560b4c57c5e8d529471d
Danar Noverawan <39963063+danarn17@users.noreply.github.com>
2020-08-07 13:20:09 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -383,13 +383,31 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+if (ENVIRONMENT !== 'production') {
+    $config['sess_driver'] = 'files';
+    $config['sess_cookie_name'] = 'ci_session';
+    $config['sess_expiration'] = 7200;
+    $config['sess_save_path'] = NULL;
+    $config['sess_match_ip'] = FALSE;
+    $config['sess_time_to_update'] = 300;
+    $config['sess_regenerate_destroy'] = FALSE;
+} elseif ((ENVIRONMENT == 'testing')) {
+    $config['sess_driver'] = 'files';
+    $config['sess_cookie_name'] = 'ci_session';
+    $config['sess_expiration'] = 7200;
+    $config['sess_save_path'] = NULL;
+    $config['sess_match_ip'] = FALSE;
+    $config['sess_time_to_update'] = 300;
+    $config['sess_regenerate_destroy'] = FALSE;
+} else {
+    $config['sess_driver'] = 'files';
+    $config['sess_cookie_name'] = 'ci_session';
+    $config['sess_expiration'] = 7200;
+    $config['sess_save_path'] = sys_get_temp_dir();
+    $config['sess_match_ip'] = FALSE;
+    $config['sess_time_to_update'] = 300;
+    $config['sess_regenerate_destroy'] = FALSE;
+}
 
 /*
 |--------------------------------------------------------------------------
