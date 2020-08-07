951234e47022e377b71ab9d350b0493466037df4
Danar Noverawan <39963063+danarn17@users.noreply.github.com>
2020-08-07 20:05:47 +0700

    fucek

--- application/controllers/House.php
+++ application/controllers/House.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 date_default_timezone_set('Asia/Jakarta');
-class Home extends CI_Controller
+class House extends CI_Controller
 {
     function __construct()
     {
