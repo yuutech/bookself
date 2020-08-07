57d47ad8af79709f3bb3f317ca1d743bd35d9967
Danar Noverawan <39963063+danarn17@users.noreply.github.com>
2020-08-07 16:12:21 +0700

    tes2

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
@@ -58,7 +58,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']    = 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -82,7 +82,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']    = 'english';
 
 /*
 |--------------------------------------------------------------------------
@@ -424,11 +424,11 @@ if (ENVIRONMENT !== 'production') {
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
+$config['cookie_prefix']    = '';
+$config['cookie_domain']    = '';
+$config['cookie_path']        = '/';
+$config['cookie_secure']    = FALSE;
+$config['cookie_httponly']     = FALSE;
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,43 +2,44 @@
 
 date_default_timezone_set('Asia/Jakarta');
 
-class home extends CI_Controller {
+class Home extends CI_Controller
+{
 
-	public function index() 
+    public function index()
     {
 
         $buku = $this->model_buku->tampildata()->result();
-		$data = array(
-			'buku' => $buku,
-		); 
+        $data = array(
+            'buku' => $buku,
+        );
         $this->load->view('template/header');
-        $this->load->view('index',$data);
+        $this->load->view('index', $data);
         $this->load->view('template/footer');
     }
     public function tambah_ke_keranjang($kd)
     {
         # code...
-        $buku= $this->model_buku->find($kd);
+        $buku = $this->model_buku->find($kd);
         $data = array(
             'id'      => $buku->kd_buku,
             'qty'     => 1,
             'price'   => $buku->halaman,
             'name'    => $buku->judul
-            
-    );
-    $this->cart->insert($data);
 
-    $id= $data['id'];
-    $qty= $data['qty'];
-    $sql = "UPDATE tb_buku SET jumlah = jumlah - $qty WHERE kd_buku = $id";
-    $this->db->query($sql);
-            
-   // var_dump($data);
-    //var_dump($a);
-    $this->session->set_flashdata('pesan','<div class="alert alert-success alert-dismissible fade show"
+        );
+        $this->cart->insert($data);
+
+        $id = $data['id'];
+        $qty = $data['qty'];
+        $sql = "UPDATE tb_buku SET jumlah = jumlah - $qty WHERE kd_buku = $id";
+        $this->db->query($sql);
+
+        // var_dump($data);
+        //var_dump($a);
+        $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show"
         role="alert">Buku berhasil dimasukkan ke keranjang!<button type="button" class="close"
         data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-    redirect ('home');
+        redirect('home');
     }
     public function detail_keranjang()
     {
@@ -56,9 +57,9 @@ class home extends CI_Controller {
     function delete_buku_cart()
     {
 
-        $id=$this->input->post('id');
-        $qty=$this->input->post('qty');
-        $rowid=$this->input->post('rowid');
+        $id = $this->input->post('id');
+        $qty = $this->input->post('qty');
+        $rowid = $this->input->post('rowid');
         $data = array(
             'rowid' => $rowid,
             'qty' => 0
@@ -67,26 +68,26 @@ class home extends CI_Controller {
         $sql = "UPDATE tb_buku SET jumlah = jumlah + $qty WHERE kd_buku = $id";
         $this->db->query($sql);
 
-         $this->cart->update($data);
-         redirect('home/detail_keranjang');
+        $this->cart->update($data);
+        redirect('home/detail_keranjang');
     }
 
     function proses_pesanan()
     {
         $processed = $this->invoice_m->index();
         $this->cart->destroy();
-        if($processed){
-        $this->load->view('template/header');
-        $this->load->view('keranjang');
-        $this->load->view('template/footer');
+        if ($processed) {
+            $this->load->view('template/header');
+            $this->load->view('keranjang');
+            $this->load->view('template/footer');
         } else {
-            $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show"
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show"
                         role="alert">gagal meminjam!<button type="button" class="close"
                         data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-                        redirect('home');
+            redirect('home');
         }
 
-        $this->session->set_flashdata('pesan','<div class="alert alert-success alert-dismissible fade show"
+        $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show"
             role="alert">Data peminjaman berhasil ditambahkan!<button type="button" class="close"
             data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
         redirect('home');
