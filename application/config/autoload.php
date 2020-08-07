ed1e832899cc5e5e907e870b1fb10d7d0a572fc9
Danar Noverawan <39963063+danarn17@users.noreply.github.com>
2020-08-07 20:43:12 +0700

    fucek

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,5 +132,5 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('user_m', 'category_m', 'model_buku', 'penerbit_m', 'invoice_m', 'count_m');
-// $autoload['model'] = array();
+// $autoload['model'] = array('user_m', 'category_m', 'model_buku', 'penerbit_m', 'invoice_m', 'count_m');
+$autoload['model'] = array();
